e586c058dc27ed7bb6484aee8da66b4463c46f7f
ryand <gravenimagerd@gmail.com>
2026-04-19 10:24:42 -0400

    Initial commit

---
+++ Valgraves.Common/Player.cs
@@ -0,0 +1,22 @@
+namespace Valgraves.Common
+{
+    public static class Player
+    {
+        private static EntityPlayer _currentPlayer = null;
+
+        public static EntityPlayer Entity
+        {
+            get
+            {
+                _currentPlayer = GameManager.Instance.World?.GetPrimaryPlayer();
+                if (_currentPlayer == null)
+                {
+                    Log.Error("Could not find primary player!");
+                }
+                return _currentPlayer;
+            }
+        }
+        
+        public static int EntityId => _currentPlayer.entityId;
+    }
+}
